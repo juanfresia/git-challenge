2fe58d41d710766edda6d90a17f5900e55c09cc6
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2020-07-02 02:19:31 -0300

    Updated solution challenge-4
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- challenge-4/check_challenge.py
+++ challenge-4/check_challenge.py
@@ -9,9 +9,10 @@ print("Checking level...")
 
 assert repo.bare, "Oops! Remote is not a bare repository."
 
-assert repo.commit('master').tree.hexsha == "adea2e5f4c6ac11a25c59a18bf3527f7c4be0445", "Wrong tree"
-assert repo.commit('master^1').hexsha == "bc255866b36b696f8f465432793d1d5bd6b48267", "Wrong commit"
-assert repo.commit('master^1').tree.hexsha == "a057730f0c670858ee3e5515cbf88972d7b44bc1", "Wrong commit"
+assert repo.commit('master').tree.hexsha == "b70e985a104f8901f898c66b518b8b2d03e34d6d", "Wrong tree"
+assert repo.commit('master~1').tree.hexsha == "65571b1bf1e8f2d061610cbad2f1645d10fe4c90", "Wrong tree"
+assert repo.commit('master~2').hexsha == "d1e8616868cf696afc8109f4e59a8be25eeb25a1", "Wrong commit"
+assert repo.commit('master~2').tree.hexsha == "b39c228897e315823321ce22c597f15d0cdbaada", "Wrong tree"
 
 print("Passed!")
 
