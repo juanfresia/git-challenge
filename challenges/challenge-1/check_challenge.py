ff753a5faf45f506d1ec1226d3d4ec171b0ea51c
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2020-07-06 00:04:12 -0300

    Refactor challenge-1
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- challenges/challenge-1/check_challenge.py
+++ challenges/challenge-1/check_challenge.py
@@ -7,7 +7,8 @@ repo = Repo("/challenge/.remote")
 print("Checking level...")
 
 assert repo.bare, "Oops! Remote is not a bare repository."
-assert repo.commit('master').tree.hexsha == "e58562979d51531c06ad23f3c196f34c94342ebc", "Wrong tree"
+assert repo.commit('master').tree.hexsha == "bb5d4918427030bb9e707ddf80f74d8f8de830aa", "Wrong tree"
+assert repo.commit('master^1').hexsha == "0f5220c5b02d5a8a5eee55ae38bf82a8eff23e9b", "Wrong commit"
 
 print("Passed!")
 
--- challenges/challenge-1/repo/tree/README.md
+++
@@ -1,8 +0,0 @@
-# Just a commit
-
-Your first task is simple, you have unstaged changes you want to commit.
-
-Just do it. Go to your repo at /challenge/repo and push them to _origin_ remote.
-
-After you are done, run the `check_challenge` command to check if you passed the challenge.
-
