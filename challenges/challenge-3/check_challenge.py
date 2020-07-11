561a141d74e0fd90d0df045af9af9c60c9f02991
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2020-07-11 20:43:28 -0300

    Use new users for challenge-3
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

--- challenges/challenge-3/check_challenge.py
+++ challenges/challenge-3/check_challenge.py
@@ -10,8 +10,8 @@ print("Checking level...")
 assert repo.bare, "Oops! Remote is not a bare repository."
 
 assert repo.commit('master').tree.hexsha == "a057730f0c670858ee3e5515cbf88972d7b44bc1", "Wrong tree"
-assert repo.commit('master^1').hexsha == "5a231d5de18ab0762d78bd0067f56cd271a375e4", "Wrong commit"
-assert repo.commit('master^1').tree.hexsha == "50cfbbc27bd13be7de55c61a22dcab458445f294", "Wrong commit"
+assert repo.commit('master~1').hexsha == "eaff07ddaf2acf046f0d9432d2ec2edd0d276cdb", "Wrong commit"
+assert repo.commit('master~1').tree.hexsha == "50cfbbc27bd13be7de55c61a22dcab458445f294", "Wrong commit"
 
 print("Passed!")
 
